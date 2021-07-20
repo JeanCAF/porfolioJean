31b7ed1d4496b42402415ddc14419b20f0c5b02e
juanogit <jcarayaf64@gmail.com>
2021-07-20 23:48:26 +0200

    crea las pantallas de home, about contact, experience, portfolio, services, y footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './UI/navbar/navbar.component';
+import { NavbarComponent } from './UI/shared/navbar/navbar.component';
+import { HomeComponent } from './UI/pages/home/home.component';
+import { AboutComponent } from './UI/pages/about/about.component';
+import { ServicesComponent } from './UI/pages/services/services.component';
+import { ExperienceComponent } from './UI/pages/experience/experience.component';
+import { PortfolioComponent } from './UI/pages/portfolio/portfolio.component';
+import { ContactComponent } from './UI/pages/contact/contact.component';
+import { FooterComponent } from './UI/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ExperienceComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
